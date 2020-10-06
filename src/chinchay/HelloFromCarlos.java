d4634b6f93711996bfda781a3254b66c64263e4d
Carlos Alberto León Chinchay <cleon@dosx0099.home>
2020-10-06 20:54:38 +0200

    Initial commit

---
+++ src/chinchay/HelloFromCarlos.java
@@ -0,0 +1,9 @@
+package chinchay;
+
+public class HelloFromCarlos {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, my name is Carlos");
+    }
+}
