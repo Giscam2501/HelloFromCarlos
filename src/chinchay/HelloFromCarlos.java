26380a9c2c28449c4e10b9d4f600050a2df7d108
LanOr <alano@ezsynergy.com>
2020-10-07 13:55:02 -0400

    Alan's changes to Carlos repo

--- src/chinchay/HelloFromCarlos.java
+++ src/chinchay/HelloFromCarlos.java
@@ -5,5 +5,6 @@ public class HelloFromCarlos {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Carlos");
+        System.out.println("Hello, my name is Alan");
     }
 }
