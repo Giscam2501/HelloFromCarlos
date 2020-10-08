4e5d1400b8db33e06abb1fc756ee64497a56e181
LanOr <alano@ezsynergy.com>
2020-10-07 21:30:56 -0400

    print message changed

--- src/chinchay/HelloFromCarlos.java
+++ src/chinchay/HelloFromCarlos.java
@@ -5,6 +5,6 @@ public class HelloFromCarlos {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Carlos");
-        System.out.println("Hello, my name is Alan");
+        System.out.println("Hello Carlos, my name is Alan");
     }
 }
